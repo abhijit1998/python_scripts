496329eb0ac51b6edfa884cf0827d62772ab54a9
abhijit1998 <abhijittudu98@gmail.com>
2020-03-02 22:46:45 +0530

    uploading python files

---
+++ havoc-10.0_end.py
@@ -0,0 +1,37 @@
+import json
+import time
+import requests
+import conf
+import datetime
+
+currentDT = datetime.datetime.now()
+
+def send_telegram_message(message):
+    """Sends message via Telegram"""
+    url = "https://api.telegram.org/" + conf.telegram_bot_id + "/sendMessage"
+    data = {
+        "chat_id": conf.telegram_chat_id,
+        "text": message
+    }
+    try:
+        response = requests.request(
+            "GET",
+            url,
+            params=data
+        )
+        print("This is the Telegram response")
+        print(response.text)
+        telegram_data = json.loads(response.text)
+        return telegram_data["ok"]
+    except Exception as e:
+        print("An error occurred in sending the alert message via Telegram")
+        print(e)
+        return False
+
+
+
+line1 = "Alert! Havoc-OS test builds for Moto G5 Plus (potter) completed at " + currentDT.strftime("%d-%m-%Y %H:%M:%S IST.\n")
+line2 = "Build will be available at https://sourceforge.net/projects/unofficial-builds/files/DO-NOT-DOWNLOAD/Havoc-OS-v3.2-"+currentDT.strftime("%Y%m%d")+"-potter-Official.zip/download after some time."
+message = line1 + line2
+telegram_status = send_telegram_message(message)
+print("This is the Telegram status:", telegram_status)
