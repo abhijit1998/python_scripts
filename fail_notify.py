293a0aa1d3cdff4a239829b9d3593c86d4bb7246
abhijit1998 <abhijittudu98@gmail.com>
2020-04-08 14:42:30 +0530

    Update fail_notify.py

--- fail_notify.py
+++ fail_notify.py
@@ -9,16 +9,19 @@ import time_count
 
 currentDT = datetime.datetime.now()
 
-def convert(seconds): 
-    seconds = seconds % (24 * 3600) 
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
     hour = seconds // 3600
     seconds %= 3600
     minutes = seconds // 60
     seconds %= 60
-    return "%dh %02dm %02ds" % (hour, minutes, seconds) 
+    return "%dh %02dm %02ds" % (hour, minutes, seconds)
 
-total_time=int(time_count.end_time)-int(time_count.start_time)
-total_execution_time=convert(total_time)
+
+total_time = int(time_count.end_time)-int(time_count.start_time)
+total_execution_time = convert(total_time)
+
 
 def send_telegram_message(message):
     """Sends message via Telegram"""
@@ -42,8 +45,15 @@ def send_telegram_message(message):
         print(e)
         return False
 
-line1 = "Alert! {} build failed at ".format(url.build_name) + currentDT.strftime("%d-%m-%Y %H:%M:%S IST.\n")
+
+line1 = "Alert! {} build failed at ".format(
+    url.build_name) + currentDT.strftime("%d-%m-%Y %H:%M:%S IST.\n")
 line2 = "Duration: {}\n".format(total_execution_time)
 line3 = "Check for errors at: {}console".format(url.build_url)
 
-if check==1:
+if check.check == 1:
+    message = line1+line2+line3
+    telegram_status = send_telegram_message(message)
+    print("This is the Telegram status:", telegram_status)
+else:
+    print("Success")
